939076ad15b043414ede36abe5d8a9dc56913873
thanhph8600 <pht456654@gmail.com>
2023-02-18 21:07:31 +0700

    first

---
+++ js/index.js
@@ -0,0 +1,90 @@
+$(document).ready(function () {
+    $('.sixth-section__content-slide').owlCarousel({
+
+        loop: true,
+        margin: 30,
+        nav: true,
+        dot: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            
+            800: {
+                items: 3
+            },
+            992: {
+                items: 4
+            }
+        }
+    })
+});
+$(document).ready(function () {
+    $('.seventh-section__slide').owlCarousel({
+
+        loop: true,
+        margin: 30,
+        nav: false,
+        dot: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+});
+
+
+var menuNav = document.getElementsByClassName("header-menu")[0].getElementsByTagName("ul")[0].getElementsByTagName("span")
+// console.log(menuNav[i])
+
+//     menuNav[i].addEventListener("click",function (event){
+//         // alert(i)
+//         console.log(menuNav[i].parentElement)
+//     } );
+var check = true;
+for (var i = 0; i < menuNav.length; i++) {
+
+    menuNav[i].addEventListener('click', ((j) => {
+
+        return function () {
+            for (var s = 0; s < menuNav.length; s++) {
+                document.getElementsByClassName("header-menu__lever2")[s].setAttribute("style", "display:none !important")
+
+            }
+            if (check == j) {
+                document.getElementsByClassName("header-menu__lever2")[j].setAttribute("style", "display:none !important")
+                check = true
+
+
+            } else {
+                document.getElementsByClassName("header-menu__lever2")[j].setAttribute("style", "display:block !important")
+                menuNav[j].style.display = "block"
+                check = j
+
+            }
+
+            // alert(j)
+            // console.log(document.getElementsByClassName("header-menu")[0].getElementsByTagName("ul")[0].getElementsByTagName("li")[j].getElementsByTagName("ul")[0])
+
+
+        }
+    })(i))
+}
+
+document.getElementsByClassName("mobile-nav")[0].getElementsByTagName("span")[0].addEventListener("click",function() {
+    document.getElementsByClassName("header-menu")[0].style.display = "none"
+})
+
+document.getElementsByClassName("fa-bars")[0].addEventListener("click",function() {
+    document.getElementsByClassName("header-menu")[0].style.display = "block"
+})
